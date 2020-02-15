2067eaccff152e6af5b770d51f259c9735c56f9e
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-02-15 18:44:53 -0300

    Added usePost custom hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+// import axios from 'axios'
 
 import useGet from './useGet'
+import usePost from './usePost'
+
 
 const url = 'https://mymoney-gusprado.firebaseio.com/movimentacoes.json'
 
 function App() {
   const data = useGet(url)
-  const data2 = useGet('http://httpbin.org/ip')
+  // const data2 = useGet('http://httpbin.org/ip')
+  const [postData, post] = usePost(url)
+
+
+  const saveNew = () => {
+    post({ valor: 15, descricao: 'ceva'})
+  }
 
   return (
     <div className="App">
       <h1>My Money</h1>
       { data.loading ? 'Loading...' : JSON.stringify(data.data) }
-      <pre>{JSON.stringify(data2)}</pre>
+      {/* <pre>{JSON.stringify(data2)}</pre> */}
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={saveNew}>Salvar</button>
     </div>
   );
 }
